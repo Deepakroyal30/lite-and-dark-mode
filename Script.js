6bf5f0290618797c70dc7de9787e07491c36e66b
Deepak <139122877+Deepakroyal30@users.noreply.github.com>
2024-01-09 20:16:24 +0530

    all files are added

---
+++ Script.js
@@ -0,0 +1,7 @@
+const body = document.body;
+const circle = document.querySelector('.circle');
+
+function toggleMode() {
+  body.classList.toggle('dark-mode');
+  circle.classList.toggle('move-right');
+ }
